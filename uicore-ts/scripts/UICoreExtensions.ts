782a09ab263dcbc56afca128a91960698c9cfe83
Mart Ernits <mart223@gmail.com>
2025-01-24 16:15:02 +0200

    Add Array.prototype.average to UICoreExtensions file

--- uicore-ts/scripts/UICoreExtensions.ts
+++ uicore-ts/scripts/UICoreExtensions.ts
@@ -44,6 +44,8 @@ declare global {
         
         min(): number;
         
+        average(): number;
+        
         isEqualToArray(array: Array<T>, keyPath?: string): boolean;
         
     }
@@ -503,6 +505,17 @@ Array.prototype.min = function () {
     return Math.min.apply(null, this)
 }
 
+if (!Array.prototype.average) {
+    
+    Array.prototype.average = function () {
+        if (this.length == 0) {
+            return 0;
+        }
+        const sum = this.reduce((a, b) => a + b, 0)
+        return sum / this.length
+    }
+    
+}
 
 // interface Array<T> {
 //
